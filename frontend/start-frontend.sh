b1a66146c259b5bdee959007a79c7498e9fce0a6
mjrweslley <buchechaguarapari@gmail.com>
2026-06-05 00:56:57 +0100

    commit

--- frontend/start-frontend.sh
+++ frontend/start-frontend.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 cd /home/mjrweslley/app/frontend
 
-# 1. Apaga a pasta de produção antiga para evitar conflitos
-rm -rf dist/
-
-# 2. Exporta o projeto web (A sintaxe moderna do Expo 50+)
-npx expo export
-
-# 3. Serve a pasta 'dist', forçando a escuta no IP público (0.0.0.0) na porta 8082
-npx serve dist -l tcp://0.0.0.0:8082 -s
+# Usa o servidor nativo do Python para servir a pasta 'dist' na porta 8082
+python3 -m http.server 8082 --directory dist
